3529d14b316e1247fd754f2df64d639fbde7e28e
Stuart-Yee <ki6fzb@gmail.com>
2021-10-12 21:31:02 -0700

    removed data models

---
+++ SeeFoodQuickly/src/main/java/com/SeeFoodQuickly/SeeFoodQuicklyApplication.java
@@ -0,0 +1,13 @@
+package com.SeeFoodQuickly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeFoodQuicklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeFoodQuicklyApplication.class, args);
+	}
+
+}
